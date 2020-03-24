4370b6f53de03838f2ebd5d6ff8fe9d83e40ef72
Viviane <vshibukawa@spiria.com>
2020-03-24 13:01:05 -0400

    add apollo-boost and first implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider} from 'react-apollo';
 import BookList from './components/BookList';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8080/graphql'
+})
 
 function App() {
   return (
     <div className="App">
       <h1>Book's List</h1>
-      <BookList/>
+      <ApolloProvider client={client}>
+        <BookList/>
+      </ApolloProvider>
     </div>
   );
 }
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+import Book from './Book';
+
+const getBooksQuery = gql`
+{
+    books{
+        name
+        id
+    }
+}
+`
 
 const BookList = () => {
+    console.log("BookList -> retorno", getBooksQuery)
 
     return(
         <div>
             <ul id='book-list'>
-                
+                <Book/>
             </ul>
         </div>
     )
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
