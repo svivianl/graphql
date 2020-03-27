db273299e014649f78ba6e0469d66b263cd9ebf8
Vivian <vlshibukawa@gmail.com>
2020-03-27 13:09:34 -0400

    add .env in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 import ApolloClient from "apollo-boost";
-import { ApolloProvider} from 'react-apollo';
-import AddBookForm from './components/addBookForm/AddBookForm';
-import BookList from './components/bookList/BookList';
+import { ApolloProvider } from "react-apollo";
+import AddBookForm from "./components/addBookForm/AddBookForm";
+import BookList from "./components/bookList/BookList";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8080/graphql'
-})
+  uri: process.env.REACT_APP_URL
+});
 
 function App() {
   return (
     <div id="main">
       <h1>Book's List</h1>
       <ApolloProvider client={client}>
-        <AddBookForm/>
-        <BookList/>
+        <AddBookForm />
+        <BookList />
       </ApolloProvider>
     </div>
   );
